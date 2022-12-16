d4966c8b5e980d492a7f3fffc07ed6b16246e8e7
radu_vele <99049451+Radu-Vele@users.noreply.github.com>
2022-12-17 01:02:31 +0200

    Updated documentation

--- vivado_project/mips16_pipeline_w_hazard_detection.srcs/sources_1/new/hazard_det_unit.vhd
+++ vivado_project/mips16_pipeline_w_hazard_detection.srcs/sources_1/new/hazard_det_unit.vhd
@@ -33,8 +33,8 @@ entity hazard_det_unit is
         Branch_instruction: in std_logic;
         ID_EX_RegWrite: in std_logic;
         EX_MEM_RegWrite: in std_logic;
-        EX_WrAddrChosen: in std_logic_vector(2 downto 0);
-        EX_MEM_WrAddrChosen: in std_logic_vector(2 downto 0);
+        EX_WrAddrChosen: in std_logic_vector(2 downto 0); -- from EX unit
+        EX_MEM_WrAddrChosen: in std_logic_vector(2 downto 0); -- from MEM unit
         IF_ID_WriteEn: out std_logic;
         Ctrl_Sel: out std_logic;
         PC_Enable: out std_logic
@@ -50,12 +50,14 @@ begin
         Ctrl_Sel <= '1';
         PC_Enable <= '1';
         
+        -- load data hazard
         if ID_EX_MemRead = '1' and (ID_Rs = ID_EX_Rt or ID_Rt = ID_EX_Rt) then
             IF_ID_WriteEn <= '0';
             Ctrl_Sel <= '0';
             PC_Enable <= '0';                    
         end if;
         
+        -- branch dependencies
         if (ID_EX_RegWrite = '1' and Branch_instruction = '1') and (ID_Rs = EX_WrAddrChosen or ID_Rt = EX_WrAddrChosen) then
             IF_ID_WriteEn <= '0';
             Ctrl_Sel <= '0';
